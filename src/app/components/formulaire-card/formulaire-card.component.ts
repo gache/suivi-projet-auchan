6cde9e99576cbb7517abb43712abf87573bd370d
erickstage <efranco@partner.auchan.fr>
2019-06-23 20:39:38 +0200

    validation du formulaire ajouter un projet

--- src/app/components/formulaire-card/formulaire-card.component.ts
+++ src/app/components/formulaire-card/formulaire-card.component.ts
@@ -12,23 +12,36 @@ export class FormulaireCardComponent implements OnInit {
 
   // @Output() formulaire: EventEmitter<any>
 
-  formulaire1: FormGroup;
-  constructor() {
-    // this.formulaire = new EventEmitter();
-    this.formulaire1 = new FormGroup({
+  formulaire1  : FormGroup;
+  description  : string;
+  Ajouter      : any;
+  nomProjet    : string;
+  messageAlert : string = "Definir le Porjet";
 
-      'nomProjet'  : new FormControl('', Validators.required ),
-      'equipe'     : new FormControl('', Validators.required ),
-      'description': new FormControl('', Validators.required )
+constructor() {
+    // this.formulaire = new EventEmitter();
+  this.formulaire1 = new FormGroup({
+
+      'nomProjet'   : new FormControl(null, [Validators.required, Validators.minLength(3)] ),
+      'equipe'      : new FormControl(null, Validators.required ),
+      'description' : new FormControl(null,
+                                      Validators.compose
+                                      ([Validators.required,
+                                        Validators.minLength(5),
+                                        Validators.maxLength(500)]) )
     });
    }
 
   ngOnInit() {
   }
 
-  ajouter() {
+  ajouter(Ajouter) {
+
     console.log(this.formulaire1.value);
+      this.description = Ajouter.description;
+      this.nomProjet   = Ajouter.nomProjet;
 
   }
 
 }
+
